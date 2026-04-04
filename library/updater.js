82a78eea3a6af335a14bc241854d605a6bbbf867
Alpha <mutengualpha@gmail.com>
2026-04-04 02:11:57 +0200

    Update updater.js

--- library/updater.js
+++ library/updater.js
@@ -1,37 +1,14 @@
 // © 2026 Alpha
 
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-const AdmZip = require('adm-zip');
+const { exec } = require("child_process");
 
-async function updateBot() {
-    try {
-        const url = "https://github.com/Alexio11-09/alpha-xmd/archive/refs/heads/main.zip";
-
-        const zipPath = path.join(__dirname, "../update.zip");
-        const extractPath = path.join(__dirname, "../update");
-
-        console.log("📥 Downloading update...");
-
-        // 📥 DOWNLOAD ZIP
-        const res = await axios.get(url, { 
-            responseType: "arraybuffer",
-            timeout: 20000
+function updateBot() {
+    return new Promise((resolve, reject) => {
+        exec("git pull origin main", (err, stdout, stderr) => {
+            if (err) return reject(stderr || err.message);
+            resolve(stdout);
         });
+    });
+}
 
-        fs.writeFileSync(zipPath, res.data);
-
-        console.log("📦 Extracting update...");
-
-        // 📦 EXTRACT
-        const zip = new AdmZip(zipPath);
-        zip.extractAllTo(extractPath, true);
-
-        const folder = fs.readdirSync(extractPath)[0];
-        const newFiles = path.join(extractPath, folder);
-
-        console.log("🔁 Updating files...");
-
-        // 🔁 COPY FILES
-        const
+module.exports = { updateBot };
